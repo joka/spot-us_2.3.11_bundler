ea1c695bc619c160ac9f607a2f0a2ba3fdfe31a6
spotus <spotus@server.scottie-lab.de>
2011-09-25 23:12:24 +0200

    this populate step misses the slug..

--- db/fixtures/shared/news_items.rb
+++ db/fixtures/shared/news_items.rb
@@ -1,20 +1,20 @@
 unless Rails.env.production?
-  1.upto 22 do |count|
-    Factory(:tip, :headline => "Tip Headline #{count}")
-  end
+  #1.upto 22 do |count|
+  #  Factory(:tip, :headline => "TTTTTTTip Headline #{count}")
+  #end
 
   tips = Tip.find :all
 
-  tips.each do |tip| 
-    2.times do
-      Factory(:pledge, :tip => tip, :amount => 25)
-    end
-  end
+  #tips.each do |tip| 
+  #  2.times do
+  #    Factory(:pledge, :tip => tip, :amount => 25)
+  #  end
+  #end
 
 
-  1.upto 22 do |count|
-    Factory(:pitch, :headline => "Pitch Headline #{count}")
-  end
+  #1.upto 22 do |count|
+  #  Factory(:pitch, :headline => "Pitch Headline #{count}")
+  #end
 
   pitches = Pitch.find :all
 
